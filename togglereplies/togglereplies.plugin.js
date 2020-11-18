7e8d956161291643538ffdcde51f10e251b02b24
Wolfie <Wolfkid200444@gmail.com>
2020-11-18 18:47:18 -0400

    nice change

---
+++ togglereplies/togglereplies.plugin.js
@@ -0,0 +1,15 @@
+/**
+ * @name togglereplies
+ * @author wolf
+ * @description Toggles on or off your mentions
+ * @version 0.0.1
+ */
+
+module.exports = class togglereplies {
+    load() {}
+
+	start() {} 
+	
+    stop() {} 
+
+}
